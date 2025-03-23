c2ab18ffb2b39d0ad9a9feec8ae5fb8b1a10198e
tolatolatop <tolatolatop@gmail.com>
2025-03-23 07:51:50 +0000

    feat: 添加快速测试命令

--- tests/data/test.sh
+++ tests/data/test.sh
@@ -1,17 +1,18 @@
 # 创建任务
 python client.py create tests/data/task_params.json tests/data/input.csv
 
+task_id=1
 # 获取任务文件
-python client.py get-file task_id tests/data/downloaded_input.csv
+python client.py get-file ${task_id} tests/data/downloaded_input.csv
 
 # 提交任务结果
-python client.py push-result task_id tests/data/task_result.json
+python client.py push-result ${task_id} tests/data/task_result.json
 
 # 获取任务结果
-python client.py get-result task_id tests/data/downloaded_result.json
+python client.py get-result ${task_id} tests/data/downloaded_result.json
 
 # 获取任务日志
-python client.py get-log task_id tests/data/downloaded_logs.json
+python client.py get-log ${task_id} tests/data/downloaded_logs.json
 
 # 发送日志内容
-cat tests/data/run.log | python client.py sender task_id
+cat tests/data/run.log | python client.py sender ${task_id}
